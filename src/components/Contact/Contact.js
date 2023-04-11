f30aac3168f668a87d15699240d1469110c7cb7c
Wojtek E <wojejs@gmail.com>
2023-04-11 23:17:05 +0200

    change some styles and update contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import './Contact.scss'
+import React from "react";
+import "./Contact.scss";
 import { FiGithub, FiLinkedin, FiTwitter } from "react-icons/fi";
-
 
 const Contact = () => {
   return (
-    <div className="app__connect">
-      <h1>Find me on</h1>
+    <div className="app__connect section__padding">
+      <h2>Find me on</h2>
       <h3>
         Feel free to <span>connect</span> with me
       </h3>
       <div className="app__connect-socials">
-        <FiGithub />
-        <FiLinkedin />
-        <FiTwitter />
+        <a href="https://github.com/ewojti" target="_blank" rel="noreferrer">
+          <FiGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/wojciech-ejsmont-95957618a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FiLinkedin />
+        </a>
+        <a href="https://twitter.com/home" target="_blank" rel="noreferrer">
+          <FiTwitter />
+        </a>
       </div>
     </div>
   );
-}
+};
 
-export default Contact
+export default Contact;
