0f9d7c25df21bde59123cb77bbea8f68cc8e1062
Wojtek E <wojejs@gmail.com>
2023-04-11 19:43:00 +0200

    add animation for header img

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,8 @@
 import React, {useEffect} from 'react';
 import images from '../../constants/images'
 import './Header.scss';
-import "aos/dist/aos.css";
-import Aos from "aos";
 
 const Header = () => {
-    useEffect(() => {
-      Aos.init({});
-    }, []);
   return (
     <div className="app__header section__padding">
       <div className="app__header-text">
@@ -16,31 +11,16 @@ const Header = () => {
         <h1 className="slide-in-fwd-center-third">
           I AM ASPIRE AS A REACT FRONTEND DEVELOPER FOR MY FIRST JOB
         </h1>
-        <p
-          className="app__header-description slide-in-bottom"
-          data-aos="fade-up"
-          data-aos-anchor-placement="center-bottom"
-          data-aos-delay="2600"
-        >
+        <p className="app__header-description slide-in-bottom">
           I CREATE WEBSITES THAT ARE A PLEASURE TO USE. I DESIGN THEM WITH
           BEAUTIFUL TYPOGRAPHY, MEANINGFUL TRANSITIONS AND THOUGHTFUL
           INTERACTIONS THAT DELIGHT PEOPLE.
         </p>
-        <button
-          className="app__header-text-button slide-in-bottom-sec"
-          // data-aos="fade-up"
-          // data-aos-delay="2800"
-        >
+        <button className="app__header-text-button slide-in-bottom-sec">
           <p>Get in touch</p>
         </button>
       </div>
-      <div
-        className="app__header-photo"
-        data-aos="fade-left"
-        data-aos-anchor="#example-anchor"
-        data-aos-offset="500"
-        data-aos-duration="1500"
-      >
+      <div className="app__header-photo text-focus-in">
         <img src={images.portfolio} alt="portfolio" />
       </div>
     </div>
